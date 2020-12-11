f76f1d44d78c3864ead9528cfd19c1a966bd3e64
gishsi <56772374+gishsi@users.noreply.github.com>
2020-12-11 20:54:14 +0100

    Update Mintris.ino

--- Mintris/Mintris.ino
+++ Mintris/Mintris.ino
@@ -1,20 +1,20 @@
 #include <AberLED.h>
 
-
 // this is an invalid state: if we are in this state, things have
 // gone badly wrong! The setup function should change this immediately.
-
 #define S_INVALID   -1
-
+#define S_START     0
+#define S_GAME      1
+#define S_END       2
 
 /*************************************************************
- * Other parts of the model than the state
+   Other parts of the model than the state
  *************************************************************/
 
 
 /*************************************************************
- * This is the state machine code given in the worksheet, which
- * you should use in your assignment too.
+   This is the state machine code given in the worksheet, which
+   you should use in your assignment too.
  *************************************************************/
 
 // the state variable - starts out invalid
@@ -31,19 +31,19 @@ void gotoState(int s) {
   // this actually changes the state
   state = s;
   // and this sets the time the new state was entered
-  stateStartTime=millis();
-}   
+  stateStartTime = millis();
+}
 
 // get the time the system has been in the current state
 
-unsigned long getStateTime(){
+unsigned long getStateTime() {
   // return the difference between the current time and
   // the time the state was entered
-  return millis()-stateStartTime;
-}       
+  return millis() - stateStartTime;
+}
 
 /*************************************************************
- * End of state machine code
+   End of state machine code
  *************************************************************/
 
 // setup puts the state machine into the first state and
@@ -52,50 +52,75 @@ unsigned long getStateTime(){
 void setup() {
   AberLED.begin();
   // remember to go to the initial state!
+  gotoState(S_START);
 }
 
 /*
- * In handleInput, we deal separately with how each state
- * handles user input using a switch statement.
- */
- 
-void handleInput(){
-  switch(state){
+   In handleInput, we deal separately with how each state
+   handles user input using a switch statement.
+*/
+
+void handleInput() {
+  switch (state) {
+    case S_START:
+      if (AberLED.getButtonDown(FIRE)) {
+        gotoState(S_GAME);
+      }
+      break;
+    case S_GAME:
+      if (AberLED.getButtonDown(FIRE)) {
+        gotoState(S_END);
+      }
+      break;
+    case S_END:
+      if (AberLED.getButtonDown(FIRE)) {
+        gotoState(S_START);
+      }
+      break;
     default:
       Serial.println("Invalid state!");
   }
 }
 
 /*
- * update deals with the model changing over time, rather than in
- * response to user input. The state is part of the model, so here
- * we deal with changing state over time.
- */
-void updateModel(){
-  switch(state){
+   update deals with the model changing over time, rather than in
+   response to user input. The state is part of the model, so here
+   we deal with changing state over time.
+*/
+void updateModel() {
+  switch (state) {
     default:
       Serial.println("Invalid state!");
   }
 }
 
 /*
- * The render function is the only place where we draw anything.
- * So here, we render different things in each state.
- */
- 
-void render(){
-  switch(state){
+   The render function is the only place where we draw anything.
+   So here, we render different things in each state.
+*/
+
+void render() {
+  switch (state) {
+    case S_START:
+      AberLED.set(4, 4, GREEN);
+      break;
+    case S_GAME:
+      AberLED.set(4, 4, YELLOW);
+      break;
+    case S_END:
+      AberLED.set(4, 4, RED);
+      break;
     default:
       Serial.println("Invalid state!");
-  }  
+  }
 }
 
 /*
- * This is the loop function, which just calls the three parts
- * of the main loop - handle input, update model, render the model.
- * I've added clear/swap around the render call so you don't need to
- * worry about putting them into render() yourself.
- */
+   This is the loop function, which just calls the three parts
+   of the main loop - handle input, update model, render the model.
+   I've added clear/swap around the render call so you don't need to
+   worry about putting them into render() yourself.
+*/
 void loop() {
   handleInput();
   updateModel();
